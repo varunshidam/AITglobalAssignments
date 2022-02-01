19925f6d406d48e51b27b79b196360d231b07db4
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-01 12:14:24 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 
-namespace CollectionGeneric
+
+namespace DemoOops
 {
 
-    class Program
+    class Circle
+    {
+        public void Calculate(double r, out double area, out double circumference)
+        {
+            area = (3.14 * r * r);
+            circumference = (2 * 3.14 * r);
+
+        }
+    }
+    class Class2
     {
         static void Main(string[] args)
         {
-            ArraySegment<String> as = new ArraySegment<string>();
-            
+            Circle c = new Circle();
+            double r, area, circumference;
+            Console.WriteLine("Enter Radius:");
+            r = Convert.ToDouble(Console.ReadLine());
+            c.Calculate(r, out area, out circumference);
+            Console.WriteLine("Area of Circle:" + area);
+            Console.WriteLine("circumference of Circle:" + circumference);
         }
     }
 }
