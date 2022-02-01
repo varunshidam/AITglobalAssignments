276967a34b0f46a3167f6546fa831a922dcecc4d
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-01 11:55:50 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,14 @@
 ﻿using System;
 
-namespace Abstractbank
+namespace CollectionGeneric
 {
-    abstract class Account
-    {
-        public abstract void CalculateIntrest();
-      
-    }
-
-    class saving : Account
-    {
-        public override void CalculateIntrest()
-        {
-            Console.WriteLine("Welcome to Saving Account");
-        }
-    }
-    class Current : Account
-    {
-        public override void CalculateIntrest()
-        {
-            Console.WriteLine("welcome to Current Account");
-        }
-    }
-    class fixDeposite : Account
-    {
-        public override void CalculateIntrest()
-        {
-            Console.WriteLine("Welcome to Fixed Deposite Account");
-        }
-    }
-
 
     class Program
     {
-        
         static void Main(string[] args)
         {
-            saving s = new saving();
-            s.CalculateIntrest();
-
-       
-            Current c = new Current();
-            c.CalculateIntrest();
-
-            fixDeposite fd = new fixDeposite();
-            fd.CalculateIntrest();
-
-
-
+            ArraySegment<String> as = new ArraySegment<string>();
+            
         }
     }
 }
