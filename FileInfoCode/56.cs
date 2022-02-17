cb7f90b28195659972b07f6c50cb8386ab173d04
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:10:22 +0530

    Add files via upload

---
+++ FileInfoCode/56.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new FileInfo("D:\\New_folder\\NewDir\\Mock3.txt");
+            if (fi.Exists)
+            {
+                Console.WriteLine(fi.FullName);
+                Console.WriteLine(fi.Length);
+                Console.WriteLine(fi.Extension);
+                Console.WriteLine(fi.CreationTime);
+            }
+            else
+            {
+                fi.Create();
+                Console.WriteLine("File Created");
+            }
+        }
+    }
+}
