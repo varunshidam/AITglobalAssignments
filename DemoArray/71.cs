e9dcaa6e5ff0cbfba06eef3d5091cef759b31bd3
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:21:30 +0530

    Add files via upload

---
+++ DemoArray/71.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoArray
+{
+    internal class Class11
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            Console.WriteLine("Enter an element 3*3");
+            for (int i = 0; i <3; i++)
+            {
+                Console.WriteLine("Enterr Elements for row");
+                for (int j = 0; j < 3; j++)
+                {
+                    arr[i, j] = int.Parse(Console.ReadLine());
+                 }
+            }
+            Console.WriteLine("Array Elements are : ");
+           for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" " + arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
