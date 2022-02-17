c2af16e587eec567bd54ddbcba003ec0b78e99a2
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:19:02 +0530

    Add files via upload

---
+++ PrimeOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            int a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine(" is a Prime Number", +n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
