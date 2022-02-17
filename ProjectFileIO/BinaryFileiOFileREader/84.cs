09c7698945d9f5e2c82e57e1c9acc46af0403e01
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:31:09 +0530

    Add files via upload

---
+++ ProjectFileIO/BinaryFileiOFileREader/84.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace BinaryReaerWriterEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("BinaryData.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(fs);
+            Console.WriteLine("Empid " + reader.ReadInt32());
+            Console.WriteLine("Ename " + reader.ReadString());
+            Console.ReadLine();
+
+        }
+    }
+}
