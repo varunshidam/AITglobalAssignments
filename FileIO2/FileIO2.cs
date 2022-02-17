dfe13e0bc462814cb7d322d8b4363459f8e6b985
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 19:56:04 +0530

    Add files via upload

---
+++ FileIO2/FileIO2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                FileStream fs = new FileStream("Demo1.text", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+
+                Console.WriteLine("Enter String ");
+                string ss = Console.ReadLine();
+
+                sw.WriteLine(ss);
+                sw.Flush();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Operation succes...");
+            }
+
+            
+
+        }
+    }
+}
