65a90ca9d3ca3306fa70155dac6ac4022610b1c1
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:12:23 +0530

    Add files via upload

---
+++ ListGenericCode/59.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListGenericCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             List<int> list = new List<int>();
+             list.Add(45);
+             list.Add(78);
+             list.Add(23);
+             list.Add(33);
+
+             foreach(int i in list)
+             {
+                 Console.WriteLine(i);
+             }
+         
+        }
+    }
+}
