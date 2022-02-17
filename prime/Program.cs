42dc8cb34778ccb237884c0c382a8c787402e7a4
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:18:18 +0530

    Add files via upload

---
+++ prime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    if (j == i)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
