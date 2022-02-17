afa596ffd08d101c381bab07610bcc2fd77bf3d5
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 19:59:41 +0530

    Add files via upload

---
+++ PalimdromeNumber/PalimdromeNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class PalimdromeNumber
+{
+    public static void Main(string[] args)
+    {
+        int no, temp = 0, t, rev;
+        Console.Write("Please provide the no: ");
+        no = int.Parse(Console.ReadLine());
+        t = no;
+        while (no > 0)
+        {
+            rev = no % 10;
+            temp = (temp * 10) + rev;
+            no = no / 10;
+        }
+        if (t == temp)
+            Console.Write(" number is Palindrome.");
+        else
+            Console.Write("is not Palindrome");
+    }
+}
