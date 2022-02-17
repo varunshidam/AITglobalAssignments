fc31ca7de6e0967101f8b80876037bd598f78cba
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:14:42 +0530

    Add files via upload

---
+++ StackNonGenericCode/62.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace StackNonGenericCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(34);
+            s.Push(67);
+            s.Push("tq");
+            s.Push(true);
+
+            foreach (object o in s)
+            {
+                Console.WriteLine(o);
+            }
+
+            s.Pop();
+            s.Pop();
+            Console.WriteLine("Data after popped elements");
+
+            foreach (object o in s)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
