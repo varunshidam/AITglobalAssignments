ba18585da77bb83ef5c44d275ea87f26430f492a
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:07:56 +0530

    Add files via upload

---
+++ DirectoryCode/52.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("D:\\New_folder\\MyDir");
+            Console.WriteLine(Directory.GetCreationTime("D:\\New_folder\\MyDir"));
+            Console.WriteLine("Directory Created");
+
+            DirectoryInfo di = new DirectoryInfo("D:\\New_folder\\NewDir");
+            if (di.Exists)
+            {
+                Console.WriteLine("Directory Exists");
+                Console.WriteLine(di.FullName);
+                Console.WriteLine(di.CreationTime);
+                Console.WriteLine(di.LastAccessTime);
+                //di.CreateSubdirectory("Subdir");
+                File.Create("D:\\New_folder\\NewDir\\DemoFile.txt");
+                Console.WriteLine("File Created");
+
+
+            }
+            else
+            {
+                di.Create();
+                Console.WriteLine("Directory Created");
+            }
+        }
+    }
+}
