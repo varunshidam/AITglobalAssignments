0a65ecd0cca9de7ae48e71aaa2422fb9f9bf2948
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:17:35 +0530

    Add files via upload

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    if (j == i)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character : ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+          
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel");
+
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
