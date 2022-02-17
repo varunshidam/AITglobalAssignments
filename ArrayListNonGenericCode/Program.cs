0423cb08408bf3d707d69fa963ee6c8cc6c3b630
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:03:32 +0530

    Add files via upload

---
+++ ArrayListNonGenericCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListNonGenericCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ArrayList list = new ArrayList();
+           list.Add(67);
+           list.Add(45.7);
+           list.Add("hello");
+           list.Add('c');
+           list.Add(true);
+           list.Insert(2, "NewVal");
+
+
+           foreach(object o in list)
+           {
+               Console.WriteLine(o);
+           }
+        }
+    }
+}
