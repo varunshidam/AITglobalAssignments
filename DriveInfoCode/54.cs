3d60b3dadb14c8d861ef7ccf0ce7efdbcce76b41
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-17 20:09:40 +0530

    Add files via upload

---
+++ DriveInfoCode/54.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace DriveInfoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo di = new DriveInfo("D:\\");
+            Console.WriteLine(di.Name);
+            Console.WriteLine(di.DriveFormat);
+            Console.WriteLine(di.VolumeLabel);
+            Console.WriteLine(di.AvailableFreeSpace);
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine(Path.GetDirectoryName("D:\\New_folder\\NewDir\\Mock3.txt"));
+            Console.WriteLine(Path.GetFileName("D:\\New_folder\\NewDir\\Mock3.txt"));
+            Console.WriteLine(Path.GetExtension("D:\\New_folder\\NewDir\\Mock3.txt"));
+            Console.WriteLine(Path.DirectorySeparatorChar);
+        }
+    }
+}
