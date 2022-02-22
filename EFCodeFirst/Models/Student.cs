75ac580a24cbc7bfd8baddcca199a53de3d119aa
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-22 14:49:24 +0530

    Add files via upload

---
+++ EFCodeFirst/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirst.Models
+{
+    public class Student
+    {
+        public int rollno { get; set; }
+        public string name { get; set; }
+        public string course { get; set; }
+
+
+    }
+}
---
+++ EFCodeFirst/Models/StudentContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirst.Models
+{
+    public class StudentContext: dbcontext
+    {
+    }
+}
