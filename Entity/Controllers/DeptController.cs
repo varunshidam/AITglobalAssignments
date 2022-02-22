d83bff31f77e71a7434f54b97d8ba25f4aff4437
varunshidam <98405861+varunshidam@users.noreply.github.com>
2022-02-22 14:38:42 +0530

    Add files via upload

---
+++ Entity/Controllers/DeptController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFramework.Models;
+
+namespace EntityFramework.Controllers
+{
+    public class DeptController : Controller
+    {
+        // GET: Dept
+        public ActionResult Index()
+        {
+            HRMSDBEntities ent = new HRMSDBEntities();
+            return View(ent.DEPARTMENTS);
+        }
+    }
+}
